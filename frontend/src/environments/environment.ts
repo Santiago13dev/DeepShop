b465dcae58247961aaef0819b1b5c5b9071e411c
kevin santiago rodriguez gomez <kevin.rodriguezgomez1308@gmail.com>
2025-10-17 13:08:46 -0500

    feat: add frontend files

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://tu-proyecto.supabase.co',
+  supabaseKey: 'tu-anon-key',
+  apiUrl: 'http://localhost:3000/api',
+  stripePublicKey: 'pk_test_tu-clave',
+};
---
+++ frontend/src/environments/nvironment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://tu-proyecto.supabase.co',
+  supabaseKey: 'tu-anon-key',
+  apiUrl: 'https://api.deepshop.com/api',
+  stripePublicKey: 'pk_live_tu-clave',
+};
